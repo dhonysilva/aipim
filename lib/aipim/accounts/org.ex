3c1e0652ed49cbdafe146bfccea93a9a679dc14b
Dhony Silva <dhony.web@gmail.com>
2024-02-21 22:56:54 -0300

    use @derive and Phoenix.Param to change id to org_is as the pk

--- lib/aipim/accounts/org.ex
+++ lib/aipim/accounts/org.ex
@@ -1,7 +1,25 @@
 defmodule Aipim.Accounts.Org do
   use Ecto.Schema
   import Ecto.Changeset
+
   @primary_key {:org_id, :id, autogenerate: true}
+
+  # What @derive does here?
+  # In Elixir, a protocol defines an API and its specific implementations.
+  # Phoenix.Param is a protocol used to convert data structures into URL parameters.
+  # By default, this protocol is used for integers, binaries, atoms, and structs.
+  # The default key :id is usually used for structs, but it's possible to define other parameters.
+
+  # For our case, we're using org_id parameter as the orgs primary key's table.
+
+  # Usefull docs links:
+
+  # https://hexdocs.pm/phoenix/Phoenix.Param.html
+  # https://hexdocs.pm/elixir/1.14/Protocol.html
+  # https://blog.appsignal.com/2023/12/05/advanced-multi-tenancy-for-elixir-applications-using-ecto.html
+
+  @derive {Phoenix.Param, key: :org_id}
+
   schema "orgs" do
     field :name, :string
     field :slug, :string
