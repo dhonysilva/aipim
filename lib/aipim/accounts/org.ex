ae3f648b39b4d3227d23ff92002998c51e564c59
Dhony Silva <dhony.web@gmail.com>
2024-02-21 22:21:21 -0300

    create org schema using the custom pk org_id

---
+++ lib/aipim/accounts/org.ex
@@ -0,0 +1,19 @@
+defmodule Aipim.Accounts.Org do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:org_id, :id, autogenerate: true}
+  schema "orgs" do
+    field :name, :string
+    field :slug, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(org, attrs) do
+    org
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
---
+++ priv/repo/migrations/20240222010954_create_orgs.exs
@@ -0,0 +1,15 @@
+# mix phx.gen.context Accounts Org orgs name:string slug:string
+
+defmodule Aipim.Repo.Migrations.CreateOrgs do
+  use Ecto.Migration
+
+  def change do
+    create table(:orgs, primary_key: false) do
+      add :org_id, :bigserial, primary_key: true
+      add :name, :string
+      add :slug, :string
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
