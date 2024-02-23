4686d2f2c6aea1415ba343f2300397739dfad3ba
Dhony Silva <dhony.web@gmail.com>
2024-02-23 16:38:36 -0300

    feat: Auth User

---
+++ lib/aipim_web/controllers/user_registration_html.ex
@@ -0,0 +1,5 @@
+defmodule AipimWeb.UserRegistrationHTML do
+  use AipimWeb, :html
+
+  embed_templates "user_registration_html/*"
+end
---
+++ lib/aipim_web/controllers/user_settings_html.ex
@@ -0,0 +1,5 @@
+defmodule AipimWeb.UserSettingsHTML do
+  use AipimWeb, :html
+
+  embed_templates "user_settings_html/*"
+end
