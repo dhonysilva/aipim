65992faaaf614379b20018241b30671517aff942
Dhony Silva <dhony.web@gmail.com>
2024-02-23 15:05:34 -0300

    gen migration to product_categories

---
+++ priv/repo/migrations/20240223180409_create_product_categories.exs
@@ -0,0 +1,8 @@
+mix(ecto.gen.migration(create_product_categories))
+# mix ecto.gen.migration create_product_categories
+defmodule Aipim.Repo.Migrations.CreateProductCategories do
+  use Ecto.Migration
+
+  def change do
+  end
+end
